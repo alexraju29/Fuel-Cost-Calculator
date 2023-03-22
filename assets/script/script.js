f2d2fcce97738d6961c7c417055a2d41512450f0
alexraju29 <alexraju29@gmail.com>
2023-03-22 16:01:24 +0530

    the feature to calculate the mileage is added

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,5 +1,5 @@
 // Calculate Button
-var cost = true
+var value = 1
 function calculate(){
 
     var price = document.getElementById("price").value
@@ -20,14 +20,19 @@ function calculate(){
     pricef=parseFloat(price)
     distancef=parseFloat(distance)
     mileagef=parseFloat(mileage)
-    if (cost){
+    if (value == 1){
         var result = distancef/mileagef*pricef;
         var finalresult = "You need "+result.toFixed(2)+"₹";
         document.getElementById("result").innerHTML = finalresult;
     }
-    else{
+    else if(value == 2){
         var result = distancef*mileagef/pricef;
         var finalresult = "You can travel "+result.toFixed(2)+"kms";
+        document.getElementById("result").innerHTML = finalresult;
+    }
+    else{
+      var result = distancef*pricef/mileagef;
+        var finalresult = "You have the Mileage "+result.toFixed(2)+"km/L";
         document.getElementById("result").innerHTML = finalresult;
     }
 
@@ -47,19 +52,23 @@ function clearScreen(){
 
 function costClick(){
   clearScreen()
-  const element = document.getElementById("cost-button");
-  const d_element = document.getElementById("distance-button");
-  if (element.className == "btn btn-outline-primary") {
-    element.className = "btn btn-primary";
-    d_element.className = "btn btn-outline-primary"
+  const cost_element = document.getElementById("cost-button");
+  const distance_element = document.getElementById("distance-button");
+  const mileage_element = document.getElementById("mileage-button");
+  if (cost_element.className == "btn btn-outline-primary") {
+    cost_element.className = "btn btn-primary";
+    distance_element.className = "btn btn-outline-primary";
+    mileage_element.className = "btn btn-outline-primary";
   } else {
-    element.className = "btn btn-primary";
-    d_element.className = "btn btn-outline-primary"
+    cost_element.className = "btn btn-primary";
+    distance_element.className = "btn btn-outline-primary"
+    mileage_element.className = "btn btn-outline-primary";
   }
   document.getElementById("main-heading").innerHTML = "Fuel Cost Calculator";
   document.getElementById("main-image-id").src = "assets/images/carbon-calculator.gif";
-  document.getElementById("label-id").innerHTML = "Enter Distance (km)";
-  cost = true
+  document.getElementById("label-id").innerHTML = "Enter Distance to cover (km)";
+  document.getElementById("label-for-mileage").innerHTML = "Enter Mileage (km/l)";
+  value = 1
 
   
 
@@ -67,18 +76,47 @@ function costClick(){
 
 function distanceClick(){
     clearScreen()
-    const c_element = document.getElementById("cost-button");
-    const element = document.getElementById("distance-button");
-  if (element.className == "btn btn-outline-primary") {
-    element.className = "btn btn-primary";
-    c_element.className = "btn btn-outline-primary"
+    const cost_element = document.getElementById("cost-button");
+    const distance_element = document.getElementById("distance-button");
+    const mileage_element = document.getElementById("mileage-button")
+  if (distance_element.className == "btn btn-outline-primary") {
+    distance_element.className = "btn btn-primary";
+    cost_element.className = "btn btn-outline-primary"
+    mileage_element.className = "btn btn-outline-primary";
   } else {
-    element.className = "btn btn-primary";
-    c_element.className = "btn btn-outline-primary"
+    distance_element.className = "btn btn-primary";
+    cost_element.className = "btn btn-outline-primary"
+    mileage_element.className = "btn btn-outline-primary";
   }
   document.getElementById("main-heading").innerHTML = "Distance Calculator";
   document.getElementById("main-image-id").src = "assets/images/distance.gif";
   document.getElementById("label-id").innerHTML = "Enter Amount you have (₹)";
-  cost = false
+  document.getElementById("label-for-mileage").innerHTML = "Enter Mileage (km/l)";
+
+  value = 2
+
+}
+
+function mileageClick(){
+    clearScreen()
+    const cost_element = document.getElementById("cost-button");
+    const distance_element = document.getElementById("distance-button");
+    const mileage_element = document.getElementById("mileage-button")
+  if (mileage_element.className == "btn btn-outline-primary") {
+    mileage_element.className = "btn btn-primary";
+    cost_element.className = "btn btn-outline-primary"
+    distance_element.className = "btn btn-outline-primary";
+
+  } else {
+    mileage_element.className = "btn btn-primary";
+    cost_element.className = "btn btn-outline-primary"
+    distance_element.className = "btn btn-outline-primary";
+
+  }
+  document.getElementById("main-heading").innerHTML = "Mileage Calculator";
+  document.getElementById("main-image-id").src = "assets/images/mileage.gif";
+  document.getElementById("label-id").innerHTML = "Enter Distance Traveled(km)";
+  document.getElementById("label-for-mileage").innerHTML = "Enter Amount of Fuel filled (₹)";
+  value = 3
 
 }
